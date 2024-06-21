9b3f6d9a7c5b746be7601dc846a9e7dce9abb47d
Shiva-maxi <sriramshivasubramanyam@gmail.com>
2024-06-21 17:49:20 +0530

    #

--- basic.ts
+++ basic.ts
@@ -26,3 +26,21 @@ d=["shiva","subbu"]
 let e:(string|number)[];
 
 e=["shiva",3,"scn",443];//can contain number and a string 
+
+//object
+
+let user={
+    username:"shiva",
+    age:20,
+    pass:true
+}
+
+
+user.username="subbu";
+
+// user.username=3; can assign onlt a string not a number cannot assign any other type
+
+user.age=29;
+
+
+// user.play=false; cannot even add new properties can only alter present properties
