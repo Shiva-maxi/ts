c89943e6229a33780447a555f77314d0bf6d0da2
Shiva-maxi <sriramshivasubramanyam@gmail.com>
2024-06-21 17:42:30 +0530

    first commit

---
+++ basic.ts
@@ -0,0 +1,28 @@
+let a="shiva";
+
+// a=1;onlt can assign a number
+
+
+let b:string;
+
+b="shiva";
+
+// b=5;//cant assign as b can only be a string 
+
+//union operator
+
+let c:string|number;
+
+c=4;
+c="s";//can assign both number or a string 
+
+//array
+
+let d:string[];//can contain only strings
+
+d=["shiva","subbu"]
+
+
+let e:(string|number)[];
+
+e=["shiva",3,"scn",443];//can contain number and a string 
